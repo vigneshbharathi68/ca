6becd051eb898e2aa112446f323389bbeb9e25a4
Vignesh Bharathi K <vigneshbharathi.k@gavstech.com>
2022-10-28 19:33:58 +0530

    Tried showing the json file in table structure but throwing some interface error

--- citation-angular-app/src/app/components/detail-table/detail-table.component.ts
+++ citation-angular-app/src/app/components/detail-table/detail-table.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { CitationService } from '../../services/citation.service'
+import { Citation } from "../../Citation"
 
 @Component({
   selector: 'app-detail-table',
   templateUrl: './detail-table.component.html',
   styleUrls: ['./detail-table.component.css']
 })
+
 export class DetailTableComponent implements OnInit {
+  citationData: Citation[] = []
 
   constructor(private citationServices: CitationService) { }
 
   ngOnInit(): void {
-    this.citationServices.getCitations().subscribe((citation) => console.log(citation))
+    this.citationServices.getCitations().subscribe((citation) => this.citationData = citation)
+  }
+  get tableHead() {
+    let result: string[] = []
+    if (this.citationData[0] && this.citationData[0].Meta_info) {
+      result = Object.keys(this.citationData[0].Meta_info)
+    }
+    return result
+  }
+
+  get tableData() {
+    let result: object[] = []
+    this.citationData.forEach(el => {
+      result.push(el.Meta_info);
+    })
+    return result
   }
 
 }
